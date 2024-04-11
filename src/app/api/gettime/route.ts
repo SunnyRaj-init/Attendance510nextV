e871bfa75eabcb39fe889a58e9e726d48e78f725
SunnyRaj-init <mysonsunnyraj@gmail.com>
2024-04-11 15:04:25 -0700

    used localeCompare in gettime route

--- src/app/api/gettime/route.ts
+++ src/app/api/gettime/route.ts
@@ -7,7 +7,9 @@ export async function POST(request:Request) {
     console.log(data)
     const result = await sql`SELECT * FROM lims WHERE id='1';`;
     const now=new Date().toTimeString().split(" ")[0];
-    if(now>=result.rows[0].start_at && now<=result.rows[0].end_at){
+    const comp1=now.localeCompare(result.rows[0].start_at)
+    const comp2=now.localeCompare(result.rows[0].end_at)
+    if(comp1>=0 && comp2<=0){
     return NextResponse.json({ accept:true,data:result }, { status: 200 })}
     else{
       return NextResponse.json({ accept:false,data:result }, { status: 200 }) 
