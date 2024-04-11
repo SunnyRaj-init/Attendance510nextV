47b1b5e3264797f09447ba0661363efe30ac6fe6
SunnyRaj-init <mysonsunnyraj@gmail.com>
2024-04-11 16:03:43 -0700

    used UTC in gettime

--- src/app/api/gettime/route.ts
+++ src/app/api/gettime/route.ts
@@ -6,7 +6,10 @@ export async function POST(request:Request) {
     const data=await request.json();
     console.log(data)
     const result = await sql`SELECT * FROM lims WHERE id='1';`;
-    const now=new Date().toTimeString().split(" ")[0];
+    const utc=new Date().toUTCString().split("2024")[1].split(" ")[1];
+    let hours=parseInt(utc.slice(0,2))
+    hours=hours<7 ? 24- 7+hours:hours-7
+    const now= hours<10 ? "0"+String(hours)+utc.slice(2) : String(hours)+utc.slice(2)
     const comp1=now.localeCompare(result.rows[0].start_at)
     const comp2=now.localeCompare(result.rows[0].end_at)
     if(comp1>=0 && comp2<=0){
